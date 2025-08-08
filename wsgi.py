4f975913458901be36db706228026b130f6017fe
angelo20011016 <angelo20011016@gmail.com>
2025-08-08 23:29:15 +0800

    feat: Add WebSocket support and prepare for deployment

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
-from app import create_app
+from app import create_app, socketio
 from config import Config
 
 # Create the Flask app instance using the factory pattern
 app = create_app(Config)
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app)
